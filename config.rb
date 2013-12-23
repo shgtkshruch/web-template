7aec7c21e6868ef9565ab7e9c875fd94141ec9f5
shgtkshruch <shgtk.shruch@gmail.com>
2013-12-23 15:45:57 +0900

    Organize directories

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "htdocs"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
+css_dir = "htdocs/css"
+sass_dir = "develop/sass"
+images_dir = "htdocs/img"
+javascripts_dir = "htdocs/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :expanded
